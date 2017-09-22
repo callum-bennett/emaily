b28cb38e5b35f43bd1ba313f74a73d4aaa3c2e0d
Callum Bennett <callum.bennett@enovation.ie>
2017-09-22 16:22:44 -0500

    Add billing and client side app' ;

--- models/User.js
+++ models/User.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
+  credits: {
+    type: Number,
+    default: 0
+  },
   googleId: String
 });
 
